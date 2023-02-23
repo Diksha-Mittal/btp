0b556a6977bd4b68c3dcca7a7ad35473da5e2578
Diksha-Mittal <74381533+Diksha-Mittal@users.noreply.github.com>
2023-02-23 23:21:39 +0530

    'basic2.0'

--- dataset.py
+++ dataset.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import keras
-from tenserflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import load_img
 from btp.utils import strings2numeric
 from btp.kerasutils import crop_resize_img
 
